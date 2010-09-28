4c2bc9a94ee759423adbc4e7c99b9bf9fea692ec
Jesse Adkins <jesserayadkins@gmail.com>
2010-09-28 13:30:04 -0700

    Purge cvs tags.
    Signed-off-by: Jesse Adkins <jesserayadkins@gmail.com>
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/Xvlibint.h
+++ src/Xvlibint.h
@@ -21,7 +21,6 @@ ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
 
 ******************************************************************/
-/* $XFree86: xc/lib/Xv/Xvlibint.h,v 1.5 2001/07/25 15:04:53 dawes Exp $ */
 
 #ifndef XVLIBINT_H
 #define XVLIBINT_H

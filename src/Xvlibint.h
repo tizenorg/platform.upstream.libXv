54851e0e72e2017c14c504459078c97d6112696b
Alan Coopersmith <alan.coopersmith@oracle.com>
2013-06-22 19:58:20 -0700

    Adopt X.Org standard code style, with consistent indentation
    Having differing indent levels of 2, 3, & 4 spaces hurts my eyes.
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/Xvlibint.h
+++ src/Xvlibint.h
@@ -46,27 +46,27 @@ SOFTWARE.
 
 #if !defined(UNIXCPP)
 #define XvGetReq(name, req) \
-        WORD64ALIGN\
-	if ((dpy->bufptr + SIZEOF(xv##name##Req)) > dpy->bufmax)\
-		_XFlush(dpy);\
-	req = (xv##name##Req *)(dpy->last_req = dpy->bufptr);\
-	req->reqType = info->codes->major_opcode;\
-        req->xvReqType = xv_##name; \
-        req->length = (SIZEOF(xv##name##Req))>>2;\
-	dpy->bufptr += SIZEOF(xv##name##Req);\
-	dpy->request++
+    WORD64ALIGN \
+    if ((dpy->bufptr + SIZEOF(xv##name##Req)) > dpy->bufmax) \
+        _XFlush(dpy); \
+    req = (xv##name##Req *)(dpy->last_req = dpy->bufptr); \
+    req->reqType = info->codes->major_opcode; \
+    req->xvReqType = xv_##name; \
+    req->length = (SIZEOF(xv##name##Req)) >> 2; \
+    dpy->bufptr += SIZEOF(xv##name##Req); \
+    dpy->request++
 
 #else  /* non-ANSI C uses empty comment instead of "##" for token concatenation */
 #define XvGetReq(name, req) \
-        WORD64ALIGN\
-	if ((dpy->bufptr + SIZEOF(xv/**/name/**/Req)) > dpy->bufmax)\
-		_XFlush(dpy);\
-	req = (xv/**/name/**/Req *)(dpy->last_req = dpy->bufptr);\
-	req->reqType = info->codes->major_opcode;\
-	req->xvReqType = xv_/**/name;\
-	req->length = (SIZEOF(xv/**/name/**/Req))>>2;\
-	dpy->bufptr += SIZEOF(xv/**/name/**/Req);\
-	dpy->request++
+    WORD64ALIGN \
+    if ((dpy->bufptr + SIZEOF(xv/**/name/**/Req)) > dpy->bufmax) \
+        _XFlush(dpy); \
+    req = (xv/**/name/**/Req *)(dpy->last_req = dpy->bufptr); \
+    req->reqType = info->codes->major_opcode; \
+    req->xvReqType = xv_/**/name; \
+    req->length = (SIZEOF(xv/**/name/**/Req)) >> 2; \
+    dpy->bufptr += SIZEOF(xv/**/name/**/Req); \
+    dpy->request++
 #endif
 
 
